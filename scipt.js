ec6638ecf48fa3597730db7a500c546514c84440
kaleabxyz <akaleab.9363@gmail.com>
2024-04-24 14:52:40 +0300

    basic page layout except product and checkout

---
+++ scipt.js
@@ -0,0 +1,7 @@
+let drop = document.getElementById("flowerdropdowncontent");
+function drop(){
+    drop.classList.add("drop-flowerdropdowncontent");
+}
+function undrop(){
+    drop.classList.remove("drop-flowerdropdowncontent");
+}
