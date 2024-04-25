d3bc02f2c5a2aa41148614588db505b62718991d
kalabkassa <kalabkassa@gmail.com>
2024-04-25 19:11:24 +0300

    fixed links for home and wishlist

--- edge/urls.py
+++ edge/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.index, name="index"),
+        path("", views.index, name="home"),
         path("aboutus/", views.aboutus, name='aboutus' ),
         path("contactus/", views.contact, name="contactus"),
         path("flower/", views.flowers, name="flower"),
