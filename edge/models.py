c18111e0113e6de2fd7431c36a08ac715cfc7470
kalabkassa <kalabkassa@gmail.com>
2024-04-26 18:31:25 +0300

    fixed the js link

--- edge/models.py
+++ edge/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+    SIZE_CHOICES = [
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+        ('XXL', 'XXL'),
+    ]
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+    size = models.CharField(max_length=3, choices=SIZE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Add price field
+    image = models.ImageField(upload_to='edge/images/')  # Add image field
+
+    def __str__(self):
+        return self.name
